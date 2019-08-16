93ff37ee580b439dd32612d74ca97a8ff22bca3b
David Miranda <david@storylog.com>
2019-08-16 16:14:07 -0400

    added custom toast notification library for when auth fails

--- project-files/assets/js/main.js
+++ project-files/assets/js/main.js
@@ -1,7 +1,13 @@
 import { init, getDataFromRootNode } from 'remakejs/dist/bundle.es6';
+import crostini from 'crostini';
 
 init({
-  debugSave: true // to implement. should console.log data on page whenever it changes.
+  debugSave: true, // to implement. should console.log data on page whenever it changes.
+  defaultSaveCallback: function (res) {
+    if (!res.success) {
+      crostini("Can't save data on this page", {type: "error"});
+    }
+  }
 });
 
 // for debugging
