47b34ca3728fd23acc434a61347464c36a8b3c5f
Andrew <55545116+android4682@users.noreply.github.com>
2019-09-21 15:36:57 +0200

    Fixed "Object.fromEntries is not a function" in "init-api-routes.js"
    Signed-off-by: Andrew <55545116+android4682@users.noreply.github.com>

--- _remake/lib/init-api-routes.js
+++ _remake/lib/init-api-routes.js
@@ -128,7 +128,7 @@ export function initApiRoutes ({app}) {
     let referrerUrlParsed = new URL(referrerUrl);
     let referrerUrlPath = referrerUrlParsed.pathname; // e.g. "/jane/todo-list/123"
     let params = getParamsFromPathname(referrerUrlPath); // e.g. { username: 'jane', id: '123' }
-    let query = Object.fromEntries(referrerUrlParsed.searchParams);
+    let query = referrerUrlParsed.searchParams;
 
     let usernameFromParams = params.username;
     let pathname = referrerUrlPath;
