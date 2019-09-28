458bd8e972a24ba85b48303131f897379beaf516
David Miranda <david@storylog.com>
2019-09-27 21:05:10 -0400

    adding data attributes to page based on the status of the page

---
+++ _remake/lib/add-remake-app-status.js
@@ -0,0 +1,28 @@
+import routeUtils from "../utils/route-utils";
+
+export function addRemakeAppStatusToPage ({html, currentUser, params}) {
+  let attributeString = createBodyAttributeString({currentUser, params});
+  return html.replace("<body", attributeString);
+}
+
+function createBodyAttributeString ({currentUser, params}) {
+  let str = "<body ";
+
+  if (currentUser) {
+    str += "data-user-logged-in ";
+  }
+
+  if (routeUtils.isBaseRoute(params)) {
+    str += "data-base-route ";
+  }
+
+  if (routeUtils.isUsernameRoute(params)) {
+    str += "data-username-route ";
+  }
+
+  if (routeUtils.isItemRoute(params)) {
+    str += "data-item-route ";
+  }
+
+  return str;
+}
