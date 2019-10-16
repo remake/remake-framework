472ca705341a90e8f825a936aea6cbe90fb9b40f
David Miranda <david@storylog.com>
2019-10-16 13:48:02 -0400

    much better, more efficient file watcher
    • recompiles ONLY a single app's JS or SASS files when files with leading underscore are changed
    • only compiles ONCE on the initial run
    • doesn't watch too many directories if the app is single-tenant

--- asset-bundler.js
+++ asset-bundler.js
@@ -1,45 +1,96 @@
+const dotenv = require("dotenv");
 const chokidar = require("chokidar");
 const shell = require("shelljs");
 const path = require("upath");
 const mkdirp = require("mkdirp");
 const fs = require("fs");
+const glob = require("glob")
 
-// files will be compiled if they're
-const watcher = chokidar.watch(["app/*/assets/**/!(_)*.*", "app/assets/**/!(_)*.*"]);
+dotenv.config({ path: "variables.env" });
+let isMultiTenant = process.env.REMAKE_MULTI_TENANT === "true";
+let globToSearch = isMultiTenant ? "app/*/assets/**" : "app/assets/**";
 
-watcher.on("all", (event, filePath) => {
-  
+
+// 1. INITIAL RUN 
+let initialFilePaths = glob.sync(globToSearch);
+initialFilePaths.forEach(function (filePath) {
+  let stats = fs.statSync(filePath);
+  processFile({filePath, stats});
+});
+
+// 2. WATCHER RUN
+const watcher = chokidar.watch(globToSearch, {
+  ignoreInitial: true,
+  alwaysStat: true
+});
+watcher.on("all", (event, filePath, stats) => {
+  processFile({filePath, stats, shouldRecompute: true});
+});
+
+
+function processFile ({filePath, stats, shouldRecompute}) {
   filePath = "./" + filePath;
 
-  let isJs = path.extname(filePath) === ".js";
-  let isSass = path.extname(filePath) === ".sass";
-  let {distDir, distFilePath, distFileName, distMinFileName} = getValidDestinationPath({filePath, isSass, isJs});
+  let isJsFile = path.extname(filePath) === ".js";
+  let isSassFile = path.extname(filePath) === ".sass";
+  let {distDir, distFilePath, distFileName, distMinFileName} = getValidDestinationPath({filePath, isJsFile, isSassFile});
+  let isUnderscoreFile = distFileName.startsWith("_");
+  let isFileToBeCopied = !stats.isDirectory() && !isJsFile && !isSassFile;
 
   // DEBUG:  
-  // console.log("path:", filePath);
-  // console.log({isJs, isSass, distFilePath, distFileName, distMinFileName});
+  // console.log("bundle file path:", filePath);
+  // console.log({isJsFile, isSassFile, isUnderscoreFile, isFileToBeCopied, filePath, distFilePath, distFileName, distMinFileName});
 
-  if (isJs) {
+  if (isJsFile && !isUnderscoreFile) {
     // using npx here to ensure babel is in our path
     shell.exec(`npx parcel build ${filePath} --out-dir ${distDir} --out-file ${distFileName} --no-minify --no-source-maps --no-content-hash`);
-
-    // minified build:
-    // shell.exec(`npx parcel build ${filePath} --out-dir ${distDir} --out-file ${distMinFileName} --no-source-maps --no-content-hash`);
   }
 
-  if (isSass) {
+  if (isSassFile && !isUnderscoreFile) {
     shell.exec(`npx sass ${filePath} ${distFilePath} --no-source-map`);
-  } 
+  }
 
-  if (!isJs && !isSass) {
+  if (isFileToBeCopied) {
     fs.copyFileSync(filePath, distFilePath);
   }
 
-});
+  // recompile all files of the same type if file starts with an underscore
+  if ((isJsFile || isSassFile) && isUnderscoreFile && shouldRecompute) {
+    recompileFilesForApp({filePath, isJsFile, isSassFile});
+  }
+}
 
-function getValidDestinationPath ({filePath, isSass, isJs}) {
+
+function recompileFilesForApp ({filePath, isJsFile, isSassFile}) {
+  let indexOfAssetsString = filePath.indexOf("/assets/");
+  let dirToSearch = filePath.slice(0, indexOfAssetsString + 8);
+
+  let globToSearch;
+  if (isJsFile) {
+    globToSearch = dirToSearch + "**/!(_)*.js";
+  } else {
+    globToSearch = dirToSearch + "**/!(_)*.sass";
+  }
+
+  let recompileFilePaths = glob.sync(globToSearch);
+  recompileFilePaths.forEach(fp => {
+    let {distDir, distFilePath, distFileName, distMinFileName} = getValidDestinationPath({filePath: fp, isSassFile, isJsFile});
+    
+    // DEBUG:
+    // console.log("Recompile file:", {fp, distDir, distFilePath, distFileName, distMinFileName});
+
+    if (isJsFile) {
+      shell.exec(`npx parcel build ${fp} --out-dir ${distDir} --out-file ${distFileName} --no-minify --no-source-maps --no-content-hash`);
+    } else {
+      shell.exec(`npx sass ${fp} ${distFilePath} --no-source-map`);
+    }
+  });
+}
+
+function getValidDestinationPath ({filePath, isSassFile, isJsFile}) {
   let distFilePath = filePath.replace("./app/", "./_remake/dist/");
-  if (isSass) {
+
+  if (isSassFile) {
     distFilePath = distFilePath.replace(".sass", ".css").replace("/sass/", "/css/");
   }
 
