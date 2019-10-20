03164fb21d29423af848546d92c493cf9aaadb77
Andrew <55545116+android4682@users.noreply.github.com>
2019-10-20 11:47:13 +0200

    Self test framework changes - Detached self test page from Remake - Loading Remake manually into the self test page - Add a npm script to generate the Remake framework for self test page - Added a step by step list of what `selftest_clientside.js` does - Added more comments to the whole file - Renamed `parse2TestFramework` to `selfTestFramework` - Renamed the argument `(testcode)` to `(test-xmp-element)` - Have split up the `convertElementsToTest` function into 3-4 parts - Created a function that goes through all the test parents to gen tests - Moved all styling to a seperate CSS file - Added a button to run the tests
    Signed-off-by: Andrew <55545116+android4682@users.noreply.github.com>

--- _remake/tests/selftest_clientside.js
+++ _remake/tests/selftest_clientside.js
@@ -1,15 +1,32 @@
 /**
- * @param {!String} htmlString A String with HTML inside of it
- * @returns {!Element} Returns converted string to HTML
- * @desc Creates a HTML element by string with HTML
+ * **WHAT DOES THIS SCRIPT DO? (in 10 simple steps)**
+ * 1. Once the HTMLElement with the id *runTests* (by default the button saying *Run all tests with class "selftest"*) is clicked it starts running the onclick function.
+ * 2. The onclick function then hides the button and starts running *selfTestFramework.convertAllTestsByClassName*
+ * 	- Which converts all the HTMLElements with the class *selftest* to tests
+ * 3. The function (*selfTestFramework.convertAllTestsByClassName*) will go through all the HTMLElements with the class *selftest* and will call *selfTestFramework.convertElementsToTest*
+ * 	- Which will convert the individual HTMLElements to tests
+ * 4. The function (*selfTestFramework.convertElementsToTest*) will get the function it has to test from the *data-selftest-test* attribute
+ * 5. Then it will extract the inner html code out of the *xmp* (tags) childs
+ * 6. Afterwards it will clear the parent's inner html to be prepared for displaying result data
+ * 7. And at last the function (*selfTestFramework.convertElementsToTest*) runs *selfTestFramework.createTestsFromArray* to go through all of the individual test codes (the inner *xmp* tag content)
+ * 8. ...to generate arguments for the test function,
+ * 9. ...execute the function
+ * 10. And getting and displaying the results
  */
-HTMLDocument.prototype.createElementByHTMLString = function(htmlString) {
-	let orphanParent = this.createElement("div")
+
+
+/**
+ * @param {!String} htmlString A String that contains HTML
+ * @returns {!Element} Returns converted string to HTML
+ * @desc Creates an HTMLElement by string including HTML
+ */
+function createElementByHTMLString(htmlString) {
+	let orphanParent = document.createElement("div")
 	orphanParent.innerHTML = htmlString.trim()
 	return orphanParent.children[0]
 }
 
-// Keep for maybe future use
+// Keep for (maybe) future use
 /**
  * @param {!HTMLElement} parent The element with children where the `childsID` has to be found in
  * @param {!String} childsID The id name of the child element you're looking for
@@ -43,58 +60,72 @@ function log(msg, logLevel = "log", dirVar) {
 }
 
 /**
- * The whole parse stuff and covert it into code Framework
+ * The whole parse stuff and covert it into code Framework (aka The Self Test Framework)
  */
-class parse2TestFramework {
+class selfTestFramework {
 	/**
-	 * @throws {Error} When the argument `(testcode)` has been found but no test code has been passed via options
-	 * @param {!String} selfTestArgumentsValue The string value of the HTML attribute 'selfTest-arguments'
-	 * @param {?Object} [options={}] Values that need special filling. (IE: 'testcode')
+	 * @generator
+	 * @throws {Error} When the argument `(test-xmp-element)` has been found but no `testCodeElement` has been passed via options
+	 * @param {!String} selfTestArgumentsValue The string value of the HTML attribute 'data-selftest-arguments'
+	 * @param {?Object} [options={}] Values that need special filling. (IE: 'testCodeElement')
+	 * *Valid options are:*
+	 * - testCodeElement | The HTML code converted into an HTMLElement that was originally in the inner XMP tag in the parent self test element
 	 * @returns {!Array} Returns array with replaced arguments
+	 * @summery Replaces all special arguments with their corresponding value.
 	 * @desc Replaces all special arguments with their corresponding value.
+	 * *Special arguments are:*
+	 * - (test-xmp-element) | Replaces that argument with the code within the xmp tag
 	 */
 	static createTestFunctionArguments(selfTestArgumentsValue, options = {}) {
+		// Prepare/Initiate the return array
 		let correctArgs = []
+		// Split the given arguments from a string to an array
 		let arrArgs = selfTestArgumentsValue.split(",")
+		// Go through all the given arguments
 		for (let i = 0; i < arrArgs.length; i++) {
 			const argument = arrArgs[i];
-			if (argument == "(testcode)") {
+			// If the current argument is the special argument (test-xmp-element)
+			if (argument == "(test-xmp-element)") {
+				// Check if the HTMLElement that has to be passed to the function is available in options
 				if (options.testCodeElement != null) {
+					// Push the HTMLElement to the return arguments array
 					correctArgs.push(options.testCodeElement)
 				} else {
-					throw new Error("(testcode) has been found as argument but no test code has been provided in the options")
+					// Throw an error since the argument is specified but no HTMLElement is available from the 'options' object
+					throw new Error("(test-xmp-element) has been found as argument but no test code has been provided in the options")
 				}
+			// If the current argument is not a special argument then just add it to the return arguments array
 			} else {
 				correctArgs.push(argument)
 			}
 		}
+		// And finally return the newly generated arguments array
 		return correctArgs
 	}
+
 	/**
-	 * @generator
-	 * @param {!String} parentID ID of the parent element that contains all the tests
-	 * @throws {Error} `parentID` must be a valid HTML id
-	 * @summary Converts all xmp innerText's to tests, executes these tests and renders a response on the page
-	 * @desc Converts all xmp innerText's to tests, executes these tests and renders a response on the page
-	 * MAKE SURE THAT THE PARENT ELEMENT HAVE THE FOLLOWING ATTRIBUTES AND ARE FILLED:
-	 * - 'selfTest-test="functionYouWantToExecute()"'
-	 * - 'selfTest-arguments="firstArgument,secondArgument,thirdArgument,etc"'
-	 * *Argument can also be:*
-	 * - '(testcode)' - *Without quotes* This will pass the test code to the set function
+	 * @param {!String} className Name of the class where the selftests are located in the html file
+	 * @desc Gets all the tests that have to be performed (aka an alias for `document.getElementsByClassName()`)
 	 */
-	static convertElementsToTest(parentID) {
-		// Get the element that contains all tests
-		let parent = document.getElementById(parentID)
-		// Throw an error is no parent is found
-		if (parent == null) throw new Error(`Couldn't find any element with the id '${parentID}'`)
-		// Getting the test function's name if the 'selfTest-test' is set
-		let testFunc = (parent.attributes['selfTest-test'] != null && parent.attributes['selfTest-test'].value != null) ? parent.attributes['selfTest-test'].value : null;
-		// Preparing to store the test codes
+	static getSelfTestElements(className = "selftest") {
+		return document.getElementsByClassName(className)
+	}
+
+	/**
+	 * @private
+	 * @param {!HTMLElement} htmlElement The parent self test HTML element
+	 * @param {!String} testFunc The name of the test function
+	 * @throws {Error} `htmlElement` must be a valid HTMLElement
+	 * @desc Gets html code out of inner XMP tag of parent (self test HTML element)
+	 */
+	static getTestCodes(htmlElement, testFunc) {
+		// Throw an error is no htmlElement is set
+		if (htmlElement == null) throw new Error(`Given HTMLElement is invalid or is not set.`)
 		let testCodes = []
 		// Find all the tests by looping through the children of the parent
 		log(`[${testFunc}] Getting all the tests...`, "info")
-		for (let i = 0; i < parent.children.length; i++) {
-			const child = parent.children[i];
+		for (let i = 0; i < htmlElement.children.length; i++) {
+			const child = htmlElement.children[i];
 			log(`[${testFunc}] Checking the following child: `, "debug", child)
 			// If child has a xmp tag and has html inside of it continue
 			if (child.localName == "xmp" && child.innerHTML.length > 0) {
@@ -102,12 +133,23 @@ class parse2TestFramework {
 				testCodes.push(child.innerHTML.trim())
 			}
 		}
-		log(`[${testFunc}] Found the following tests: `, "log", testCodes)
+		return testCodes
+	}
+
+	/**
+	 * @private
+	 * @param {!HTMLElement} htmlElement The parent self test HTML element
+	 * @throws {Error} `htmlElement` must be a valid HTMLElement
+	 * @desc Initiates the front end to display results of the given tests
+	 */
+	static initiateSelfTestFrontEnd(htmlElement) {
+		// Throw an error is no htmlElement is set
+		if (htmlElement == null) throw new Error(`Given HTMLElement is invalid or is not set.`)
 		// Clearing the parent div for new generate code
-		parent.innerHTML = ""
+		htmlElement.innerHTML = ""
 		// Creating headers for the preview and result boxes (which will come soon)
 		let headerDiv = document.createElement("div")
-		headerDiv.className = "headerTest leaveSomeRoomForJesus"
+		headerDiv.classList.add("header-tests", "leave-some-room-for-jesus")
 		headerDiv.style.display = "flex"
 		headerDiv.style.textAlign = "center"
 		let previewH4 = document.createElement("h4")
@@ -117,8 +159,40 @@ class parse2TestFramework {
 		// Add the heading tags to the header div and the header div to the parent element
 		headerDiv.append(previewH4)
 		headerDiv.append(resultsH4)
-		parent.append(headerDiv)
-		// Start going through all test codes, make output containers, convert special arguments (if needed) and test the function
+		htmlElement.append(headerDiv)
+	}
+
+	/**
+	 * @private
+	 * @param {!HTMLElement} htmlElement The parent self test HTML element
+	 * @param {!String} testFunc The name of the test function
+	 * @param {!Object} options An object with options for `createTestFunctionArguments()`
+	 * @see selfTestFramework.createTestFunctionArguments
+	 * @desc Initiates the front end to display results of the given tests
+	 */
+	static argumentGenerator(htmlElement, testFunc, options) {
+		let args = []
+		// Check if arguments are needed to be passed to the function
+		if (htmlElement.getAttribute('data-selftest-arguments') != null) {
+			// Convert any special arguments to their correct value
+			// Also supply certain special argument values (when necessary)
+			log(`[${testFunc}] Creating arguments for test code`, "debug")
+			args = selfTestFramework.createTestFunctionArguments(htmlElement.getAttribute('data-selftest-arguments'), options)
+			log(`[${testFunc}] Arguments created for test code`, "debug", args)
+		} else {
+		}
+		return args
+	}
+
+	/**
+	 * @private
+	 * @param {!HTMLElement} htmlElement The parent self test HTML element
+	 * @param {!String} testFunc The name of the test function
+	 * @param {!Array} testCodes Array with test html strings
+	 * @see selfTestFramework.argumentGenerator
+	 * @desc Initiates the front end to display results of the given tests
+	 */
+	static createTestsFromArray(htmlElement, testFunc, testCodes) {
 		log(`[${testFunc}] Going through all the test codes...`, "info")
 		for (let i = 0; i < testCodes.length; i++) {
 			const testCode = testCodes[i];
@@ -144,15 +218,10 @@ class parse2TestFramework {
 				if (window[testFunc] != null) {
 					// Try to setup arguments to pass to the function
 					try {
-						let args = []
-						// Check if arguments are needed to be passed to the function
-						if (parent.attributes['selfTest-arguments'] != null && parent.attributes['selfTest-arguments'].value != null) {
-							// Convert any special arguments to their correct value
-							// Also supply certain special argument values (when necessary)
-							log(`[${testFunc}] Creating arguments for test code`, "debug")
-							args = parse2TestFramework.createTestFunctionArguments(parent.attributes['selfTest-arguments'].value, {testCodeElement: document.createElementByHTMLString(testCode)})
-							log(`[${testFunc}] Arguments created for test code`, "debug", args)
-						}
+
+						// Get arguments from HTML parent attribute (if any)
+						let args = selfTestFramework.argumentGenerator(htmlElement, testFunc, {testCodeElement: createElementByHTMLString(testCode)})
+
 						try {
 							// Running the actual test and stringifying the results for the front-end
 							log(`[${testFunc}] Running test with test code`, "debug")
@@ -183,7 +252,7 @@ class parse2TestFramework {
 			}
 			// Create the passing element
 			let passingElem = document.createElement("p")
-			passingElem.className = "testPassMsg leaveSomeRoomForJesus"
+			passingElem.classList.add("test-pass-message", "leave-some-room-for-jesus")
 			// Set the passing message from earlier
 			passingElem.innerText = passing.msg
 			// Set the passing color from earlier
@@ -193,11 +262,65 @@ class parse2TestFramework {
 			// Add the result box to the output container
 			outputContainer.append(resultBox)
 			// Add the output container to the parent
-			parent.append(outputContainer)
+			htmlElement.append(outputContainer)
 			// Add the passing element to the parent
-			parent.append(passingElem)
+			htmlElement.append(passingElem)
 		}
+	}
+
+	/**
+	 * @generator
+	 * @param {!HTMLElement} htmlElement The selftest parent HTML element
+	 * @throws {Error} `htmlElement` must be a valid HTMLElement
+	 * @summary Converts all xmp innerText's to tests, executes these tests and renders a response on the page
+	 * @see selfTestFramework.getTestCodes
+	 * @see selfTestFramework.initiateSelfTestFrontEnd
+	 * @see selfTestFramework.createTestsFromArray
+	 * @desc Converts all xmp innerText's to tests, executes these tests and renders a response on the page
+	 * MAKE SURE THAT THE PARENT ELEMENT HAVE THE FOLLOWING ATTRIBUTES AND ARE FILLED:
+	 * - 'data-selftest-test="functionYouWantToExecute"' | (Optional) defaults to *getDataFromRootNode*
+	 * - 'data-selftest-arguments="firstArgument,secondArgument,thirdArgument,etc"'
+	 * *Argument can also be:*
+	 * - '(test-xmp-element)' - *Without quotes* This will pass the inner xmp code to the function
+	 */
+	static convertElementsToTest(htmlElement) {
+		// Throw an error is no htmlElement is set
+		if (htmlElement == null) throw new Error(`Given HTMLElement is invalid or is not set.`)
+		// Getting the test function's name if the 'data-selftest-test' is set, defaults to the getDataFromRootNode() function
+		let testFunc = (htmlElement.getAttribute('data-selftest-test') != null) ? htmlElement.getAttribute('data-selftest-test') : "getDataFromRootNode";
+		
+		// Fetch testCodes
+		let testCodes = selfTestFramework.getTestCodes(htmlElement, testFunc)
+		log(`[${testFunc}] Found the following tests: `, "log", testCodes)
+
+		// Initiate front end html
+		selfTestFramework.initiateSelfTestFrontEnd(htmlElement)
+
+		// Start going through all test codes, make output containers, convert special arguments (if needed) and test the function
+		selfTestFramework.createTestsFromArray(htmlElement, testFunc, testCodes)
+		
+		// Finished! Results are displayed on page.
 		log(`[${testFunc}] Job's done!`, "info")
+	}
+
+	/**
+	 * @param {!String} className Name of the class where the selftests are located in the html file
+	 * @see selfTestFramework.getSelfTestElements
+	 * @see selfTestFramework.convertElementsToTest
+	 * @desc Loops through all the HTML elements with the same class name to convert them to tests and displaying it on the front end
+	 */
+	static convertAllTestsByClassName(className = "selftest") {
+		// Gets all test parents by class name
+		let allTestParents = selfTestFramework.getSelfTestElements(className)
+		// Loops through all the test parents
+		for (let i = 0; i < allTestParents.length; i++) {
+			const testParent = allTestParents[i];
+			log(`Preparing test for following HTML element: `, "debug", testParent)
+			// Converting current test parent to a test
+			selfTestFramework.convertElementsToTest(testParent)
+		}
+		// Done!
+		log("All tests have been converted!", "info")
 	}
 }
 
@@ -208,7 +331,12 @@ function selfTest_selfTest() {
 	console.log("What are you trying to do? Of course I work, duh!")
 }
 
-// When the page is loaded run the tests!
-window.onload = function() {
-	parse2TestFramework.convertElementsToTest("parse2TestDiv")
+/**
+ * @desc When an HTMLElement with the ID "runTests" gets clicked we convert all "selftest" classes to tests
+ */
+document.getElementById("runTests").onclick = function(event) {
+	// Hide the element
+	document.getElementById("runTests").style.display = "none"
+	// Start converting all test parents to tests
+	selfTestFramework.convertAllTestsByClassName("selftest")
 }
