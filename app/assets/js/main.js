73e77b08e0d9624ba524c4850805de1c83a5930e
David Miranda <david@storylog.com>
2019-10-31 16:47:44 -0400

    turn off sortable by default

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,17 +1,8 @@
 import Remake from 'remake-framework';
 import './helpers/_event-helpers';
 import crostini from 'crostini';
-import sortablejs from 'sortablejs';
 
 Remake.init({
-  sortable: {
-    sortablejs: sortablejs,
-    // note:
-    // each selector is used as a list selector AND a *group name*,
-    // which means if the selectors of two elements match, 
-    // items can be dragged between those elements
-    selectors: [".todo-lists"]
-  },
   logDataOnSave: true, // to implement. should console.log data on page whenever it changes.
   defaultSaveCallback: function (res) {
     if (!res.success) {
