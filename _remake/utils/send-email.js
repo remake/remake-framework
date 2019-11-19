3e3d13c4bba104528082785a2c67b839a89a1ae5
David Miranda <david@storylog.com>
2019-11-19 10:58:03 -0500

    fix bug

--- _remake/utils/send-email.js
+++ _remake/utils/send-email.js
@@ -1,3 +1,37 @@
+const Mailgun = require('mailgun-js');
+
 export function sendEmail ({email, subject, body}) {
-  console.log("email:", {email, subject, body});
+
+  const apiKey = process.env.MAILGUN_API_KEY;
+  const domain = process.env.MAILGUN_DOMAIN;
+  const fromEmail = process.env.EMAIL_FROM;
+
+  if (!apiKey || !domain || !fromEmail) {
+    console.log("------------------------------------------------");
+    console.log(`               === EMAIL ===`);
+    console.log(``);
+    console.log(`   FROM: ${fromEmail}`);
+    console.log(`     TO: ${email}`);
+    console.log(`SUBJECT: ${subject}`);
+    console.log(`   BODY:`);
+    console.log(body);
+    console.log(``);
+    console.log("------------------------------------------------");
+    return;
+  }
+
+  const mailgun = new Mailgun({apiKey, domain});
+
+  mailgun.messages().send({
+    from: fromEmail,
+    to: email,
+    subject: subject,
+    html: body
+  }, function (err, body) {
+    if (err) {
+      console.log("ERROR: COULDN'T SEND EMAIL");
+    } else {
+      console.log("SUCCESS! EMAIL SENT!");
+    }
+  });
 }
