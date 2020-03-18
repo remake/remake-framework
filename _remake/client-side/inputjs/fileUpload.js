deac0818670d86de61613a0574f29da89bcce4a4
David Miranda <david@storylog.com>
2020-03-18 01:06:53 -0400

    Update fileUpload.js

--- _remake/client-side/inputjs/fileUpload.js
+++ _remake/client-side/inputjs/fileUpload.js
@@ -1,7 +1,20 @@
 import { $ } from '../queryjs';
+import { ajaxFileUpload } from '../hummingbird/lib/ajax';
 
 export default function () {
   $.on("change", "input[type='file'][data-i]", function (event) {
-    console.log(23, arguments);
+    let fileInputElem = event.target;
+
+    fileInputElem.disabled = true;
+
+    ajaxFileUpload({
+      fileInputElem,
+      onProgress: function (percentage) {
+
+      }
+    });
+
+
+    // fileInputElem.disabled = false;
   });
 }
