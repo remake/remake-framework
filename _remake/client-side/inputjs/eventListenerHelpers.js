f21f874327ee07070f86cd324b9a4851b93afaba
David Miranda <david@storylog.com>
2020-11-20 12:05:22 -0500

    add [prevent-default] helper attribute

---
+++ _remake/client-side/inputjs/eventListenerHelpers.js
@@ -0,0 +1,8 @@
+export default function initEventListenerHelpers () {
+  // helper: prevent default
+  document.addEventListener("click", (event) => {
+    if (event.target.closest("[prevent-default]")) {
+      event.preventDefault();
+    }
+  });
+}
