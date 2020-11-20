ec86906edaca9be8796dd24935ae1b6d2dfb839f
David Miranda <david@storylog.com>
2020-11-20 12:07:51 -0500

    added new [run-watch] attribute to run watch functions when page loads

---
+++ _remake/client-side/inputjs/runWatchFunctions.js
@@ -0,0 +1,5 @@
+export default function runWatchFunctions () {
+  // run watch functions that have "run:watch" on them when page loads
+  let watchElems = Array.from(document.querySelectorAll("[run\\:watch]"));
+  Remake.callWatchFunctionsOnElements(watchElems);
+}
