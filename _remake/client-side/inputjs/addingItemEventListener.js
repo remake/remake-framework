bfeabed9dd38adc7a492c513b8c6a38b169d8436
Aiden Bai <mail.contraband@gmail.com>
2021-04-10 13:08:45 -0700

    Handle no attribute and multiple attribute cases for edit open-popup

--- _remake/client-side/inputjs/addingItemEventListener.js
+++ _remake/client-side/inputjs/addingItemEventListener.js
@@ -1,21 +1,22 @@
-import { $ } from '../queryjs';
-import { getAttributeValueAsArray } from '../parse-data-attributes';
-import { ajaxPost } from '../hummingbird/lib/ajax';
-import { findNearest, onAttributeEvent } from '../hummingbird/lib/dom';
-import { isStringANumber } from '../hummingbird/lib/string';
-import { showError } from '../common/show-error';
-import { callSaveFunction } from './onSave';
-import { callOnAddItemCallbacks, openEditCallback } from './callbacks';
-import optionsData from './optionsData';
-const camelCase = require('lodash/camelCase');
+import {$} from "../queryjs";
+import {getAttributeValueAsArray} from "../parse-data-attributes";
+import {ajaxPost} from "../hummingbird/lib/ajax";
+import {findNearest, onAttributeEvent} from "../hummingbird/lib/dom";
+import {isStringANumber} from "../hummingbird/lib/string";
+import {showError} from "../common/show-error";
+import {callSaveFunction} from "./onSave";
+import {callOnAddItemCallbacks} from "./callbacks";
+import {openEditCallback} from "./editableAttribute.js";
+import optionsData from "./optionsData";
+const camelCase = require("lodash/camelCase");
 
-function _defaultAddItemCallback({ templateName, listElem, whereToInsert, openPopup }) {
+function _defaultAddItemCallback({templateName, listElem, whereToInsert, openPopup}) {
   // pass the template name into an endpoint and get the resulting html back
-  ajaxPost('/new', { templateName }, function(ajaxResponse) {
-    let { htmlString, success } = ajaxResponse;
+  ajaxPost("/new", {templateName}, function(ajaxResponse) {
+    let {htmlString, success} = ajaxResponse;
 
     if (!success) {
-      callOnAddItemCallbacks({ success: false, templateName, ajaxResponse });
+      callOnAddItemCallbacks({success: false, templateName, ajaxResponse});
       return;
     }
 
@@ -31,60 +32,61 @@ function _defaultAddItemCallback({ templateName, listElem, whereToInsert, openPo
     callSaveFunction(listElem);
 
     let itemElem =
-      whereToInsert === 'afterbegin' ? listElem.firstElementChild : listElem.lastElementChild;
+      whereToInsert === "afterbegin" ? listElem.firstElementChild : listElem.lastElementChild;
 
     if (openPopup) {
       const attributeNames = itemElem.getAttributeNames();
-      let editAttribute = null;
 
-      // We need to find the full name of the edit:item attribute so
+      // We need to find the full name of the edit:item attribute(s) so
       // That we can pass it into the matchingAttribute property
       // We can't just getAttribute since edit:item's item can vary
-      for (const attributeName of attributeNames) {
-        if (attributeName.startsWith('edit:')) {
-          editAttribute = attributeName;
-          break;
-        }
-      }
+      let editAttributes = attributeNames.filter(name => name.startsWith("edit:"));
 
-      // Brings up the edit popup
-      // We need to pass a matches object because it technically relies on the hummingbird return for the
-      // callback for onAttributeEvent()
-      openEditCallback([
-        {
+      // Only open if there are edit attributes
+      if (editAttributes.length > 0) {
+        // We need to create a matches object because it technically relies
+        // on the hummingbird return for the callback for onAttributeEvent()
+        const defaultMatchObject = {
           matchingElement: itemElem,
-          matchingAttribute: editAttribute,
-          value: '',
-          eventType: 'click',
-          mathcingPartialAttributeString: 'edit:',
-        },
-      ]);
+          value: "",
+          eventType: "click",
+          matchingPartialAttributeString: "edit:",
+        };
+
+        const matches = editAttributes(attrName => ({
+          ...defaultMatchObject,
+          matchingAttribute: attrName,
+        }));
+
+        // Brings up the edit popup
+        openEditCallback(matches);
+      }
     }
 
-    callOnAddItemCallbacks({ success: true, listElem, itemElem, templateName, ajaxResponse });
+    callOnAddItemCallbacks({success: true, listElem, itemElem, templateName, ajaxResponse});
   });
 }
 
 export default function() {
   onAttributeEvent({
-    eventTypes: ['click'],
-    partialAttributeStrings: ['new:'],
-    filterOutElemsInsideAncestor: '[disable-events]',
-    callback: ({ matchingElement, matchingAttribute }) => {
-      let templateName = camelCase(matchingAttribute.substring('new:'.length));
+    eventTypes: ["click"],
+    partialAttributeStrings: ["new:"],
+    filterOutElemsInsideAncestor: "[disable-events]",
+    callback: ({matchingElement, matchingAttribute}) => {
+      let templateName = camelCase(matchingAttribute.substring("new:".length));
       // possible values in argArray: top/bottom or some selector
       let argArray = getAttributeValueAsArray(matchingElement, matchingAttribute);
-      let position = argArray.indexOf('top') !== -1 ? 'top' : 'bottom';
-      let openPopup = argArray.indexOf('open-popup') !== -1;
-      let whereToInsert = position === 'top' ? 'afterbegin' : 'beforeend';
-      let selector = argArray.find((arg) => arg !== 'top' && arg !== 'bottom') || '[array]';
+      let position = argArray.indexOf("top") !== -1 ? "top" : "bottom";
+      let openPopup = argArray.indexOf("open-popup") !== -1;
+      let whereToInsert = position === "top" ? "afterbegin" : "beforeend";
+      let selector = argArray.find(arg => arg !== "top" && arg !== "bottom") || "[array]";
       // find the nearest element matching the selector (searching through ancestors consecutively)
-      let listElem = findNearest({ elem: matchingElement, selector });
+      let listElem = findNearest({elem: matchingElement, selector});
 
       if (!optionsData._defaultAddItemCallback) {
-        _defaultAddItemCallback({ templateName, listElem, whereToInsert, openPopup });
+        _defaultAddItemCallback({templateName, listElem, whereToInsert, openPopup});
       } else {
-        optionsData._defaultAddItemCallback({ templateName, listElem, whereToInsert, openPopup });
+        optionsData._defaultAddItemCallback({templateName, listElem, whereToInsert, openPopup});
       }
     },
   });
--- _remake/client-side/inputjs/editableAttribute.js
+++ _remake/client-side/inputjs/editableAttribute.js
@@ -1,11 +1,130 @@
-import { $ } from '../queryjs';
-import { forEachAttr, onAttributeEvent } from '../hummingbird/lib/dom';
-import { copyLayout } from '../copy-layout';
-import { getClosestElemWithKey, setValueForKeyName, getKeyNamesFromElem } from '../data-utilities';
-import { openEditCallback } from './callbacks';
-import { syncDataNextTick } from './syncData';
-import { showError } from '../common/show-error';
-import autosize from '../vendor/autosize';
+import {$} from "../queryjs";
+import {forEachAttr, onAttributeEvent} from "../hummingbird/lib/dom";
+import {copyLayout} from "../copy-layout";
+import {getClosestElemWithKey, setValueForKeyName, getKeyNamesFromElem} from "../data-utilities";
+import {syncDataNextTick} from "./syncData";
+import {showError} from "../common/show-error";
+import autosize from "../vendor/autosize";
+
+// USAGE EXAMPLES:
+// edit:example-key
+// edit:example-key:text
+// edit:example-key:textarea
+export const openEditCallback = matches => {
+  // matches: [{eventType, matchingElement, matchingAttribute, matchingPartialAttributeString}]
+
+  // editableConfig: [{keyName, formType, removeOption, eventType, matchingElement, matchingAttribute, matchingPartialAttributeString}]
+  let editableConfig = matches.map(
+    ({eventType, matchingElement, matchingAttribute, matchingPartialAttributeString}) => {
+      let attributeParts = matchingAttribute.split(":");
+      // attributeParts could be:
+      // ["edit", "example-key"]
+      // ["edit", "example-key", "without-remove"]
+      // ["edit", "example-key", "textarea", "without-remove"]
+      // -> first two items are requireed, the last two are optional
+      let [_, keyName, ...otherOptions] = attributeParts;
+
+      let validRemoveOptions = ["with-remove", "without-remove", "with-erase"];
+      let validFormTypes = ["text", "textarea"];
+
+      // if `otherOptions` includes includes a valid remove option, use that. Otherwise, use default value.
+      let removeOption =
+        validRemoveOptions.find(str => otherOptions.includes(str)) || "with-remove";
+      // if `otherOptions` includes includes a valid form type option, use that. Otherwise, use default value.
+      let formType = validFormTypes.find(str => otherOptions.includes(str)) || "text";
+
+      return {
+        keyName,
+        formType,
+        removeOption,
+        eventType,
+        matchingElement,
+        matchingAttribute,
+        matchingPartialAttributeString,
+      };
+    }
+  );
+
+  // get first matching element for positioning popover
+  let firstMatch = editableConfig[0];
+  let firstMatchElem = firstMatch.matchingElement;
+  let firstMatchKeyName = firstMatch.keyName;
+  let firstMatchRemoveOption = firstMatch.removeOption;
+  let firstMatchTargetElem = getClosestElemWithKey({
+    elem: firstMatchElem,
+    keyName: firstMatchKeyName,
+  });
+
+  if (!firstMatchElem || !firstMatchKeyName || !firstMatchTargetElem) {
+    showError(
+      `Problem with the 'edit:' attribute on one of these elements:`,
+      matches.map(m => m.matchingElement)
+    );
+    return;
+  }
+
+  let editablePopoverElem = document.querySelector(".remake-edit");
+
+  // reset popover: remove old keys
+  removeObjectKeysFromElem({elem: editablePopoverElem});
+
+  // open popover
+  let hasRemove = firstMatchRemoveOption === "with-remove";
+  let hasErase = firstMatchRemoveOption === "with-erase";
+  editablePopoverElem.setAttribute(`temporary:key:remake-edit-popover`, "");
+  editablePopoverElem.setAttribute(`temporary:key:remake-edit-option-has-remove`, "");
+  editablePopoverElem.setAttribute(`temporary:key:remake-edit-option-has-erase`, "");
+  setValueForKeyName({elem: editablePopoverElem, keyName: "remake-edit-popover", value: "on"});
+  setValueForKeyName({
+    elem: editablePopoverElem,
+    keyName: "remake-edit-option-has-remove",
+    value: hasRemove ? "on" : "off",
+  });
+  setValueForKeyName({
+    elem: editablePopoverElem,
+    keyName: "remake-edit-option-has-erase",
+    value: hasErase ? "on" : "off",
+  });
+
+  // keep track of the source of the data on the popover element
+  $.data(editablePopoverElem, "source", firstMatchTargetElem);
+
+  // add object keys for storing data that's being edited in the popover
+  addObjectKeysToElem({elem: editablePopoverElem, config: editableConfig});
+
+  // sync data from the page into the popover
+  syncDataNextTick({
+    sourceElement: firstMatchElem,
+    targetElement: editablePopoverElem,
+    keyNames: editableConfig.map(obj => obj.keyName),
+    shouldSyncIntoUpdateElems: true,
+  });
+
+  // render html inside the edit popover
+  let remakeEditAreasElem = editablePopoverElem.querySelector(".remake-edit__edit-areas");
+  remakeEditAreasElem.innerHTML = generateRemakeEditAreas({config: editableConfig});
+
+  // copy the layout
+  copyLayout({
+    sourceElem: firstMatchElem,
+    targetElem: editablePopoverElem,
+    dimensionsName: "width",
+    xOffset: 0,
+    yOffset: 0,
+  });
+
+  // autosize textareas -- not sure why or even if this needs to be in a setTimeout
+  setTimeout(function() {
+    let textareas = Array.from(editablePopoverElem.querySelectorAll("textarea"));
+    textareas.forEach(el => autosize(el));
+  });
+
+  // focus first focusable element
+  let firstFormInput = editablePopoverElem.querySelector("textarea, input");
+  if (firstFormInput) {
+    firstFormInput.focus();
+  }
+};
 
 // USAGE EXAMPLES:
 // edit:example-key
@@ -13,30 +132,30 @@ import autosize from '../vendor/autosize';
 // edit:example-key:textarea
 export default function() {
   onAttributeEvent({
-    eventTypes: ['click'],
-    partialAttributeStrings: ['edit:'],
+    eventTypes: ["click"],
+    partialAttributeStrings: ["edit:"],
     groupMatchesIntoSingleCallback: true,
-    filterOutElemsInsideAncestor: '[disable-events]',
+    filterOutElemsInsideAncestor: "[disable-events]",
     callback: openEditCallback,
   });
 
   // sync data from popover into the page
-  $.on('submit', '[sync]', (event) => {
+  $.on("submit", "[sync]", event => {
     event.preventDefault();
-    let syncElement = event.currentTarget.closest('[sync]');
+    let syncElement = event.currentTarget.closest("[sync]");
     syncDataNextTick({
       sourceElement: syncElement,
-      targetElement: $.data(syncElement, 'source'),
+      targetElement: $.data(syncElement, "source"),
       keyNames: getKeyNamesFromElem(syncElement),
     });
   });
 
   // this was causing a bug before. i think the form was submitting when it shouldn't have.
-  $.on('click', ".remake-edit__button:not([type='submit'])", function(event) {
+  $.on("click", ".remake-edit__button:not([type='submit'])", function(event) {
     event.preventDefault();
   });
 
-  document.addEventListener('keydown', (event) => {
+  document.addEventListener("keydown", event => {
     // esc key
     if (event.keyCode === 27) {
       let turnedOnEditablePopover = document.querySelector('[key:remake-edit-popover="on"]');
@@ -44,45 +163,45 @@ export default function() {
       if (turnedOnEditablePopover) {
         setValueForKeyName({
           elem: turnedOnEditablePopover,
-          keyName: 'remake-edit-popover',
-          value: 'off',
+          keyName: "remake-edit-popover",
+          value: "off",
         });
       }
     }
   });
 }
 
-function removeObjectKeysFromElem({ elem }) {
+function removeObjectKeysFromElem({elem}) {
   let attributesToRemove = [];
   forEachAttr(elem, (attrName, attrValue) => {
-    if (attrName.startsWith('key:') || attrName.startsWith('temporary:key:')) {
+    if (attrName.startsWith("key:") || attrName.startsWith("temporary:key:")) {
       attributesToRemove.push(attrName);
     }
   });
   // this is outside the loop because you can't remove items from an array when you're looping through it
-  attributesToRemove.forEach((attrName) => elem.removeAttribute(attrName));
+  attributesToRemove.forEach(attrName => elem.removeAttribute(attrName));
 }
 
 // config: [{keyName, formType, removeOption, eventType, matchingElement, matchingAttribute, matchingPartialAttributeString}]
-function addObjectKeysToElem({ elem, config }) {
-  config.forEach((obj) => {
-    elem.setAttribute(`temporary:key:${obj.keyName}`, '');
+function addObjectKeysToElem({elem, config}) {
+  config.forEach(obj => {
+    elem.setAttribute(`temporary:key:${obj.keyName}`, "");
   });
 }
 
 // config: [{keyName, formType, removeOption, eventType, matchingElement, matchingAttribute, matchingPartialAttributeString}]
-function generateRemakeEditAreas({ config }) {
-  let outputHtml = '';
+function generateRemakeEditAreas({config}) {
+  let outputHtml = "";
 
   // formType can be "text" or "textarea" or something else that's not implemented yet
-  config.forEach(({ formType, keyName }) => {
+  config.forEach(({formType, keyName}) => {
     let formFieldHtml;
 
-    if (formType === 'text') {
+    if (formType === "text") {
       formFieldHtml = `<input class="remake-edit__input" update:${keyName} type="text">`;
     }
 
-    if (formType === 'textarea') {
+    if (formType === "textarea") {
       formFieldHtml = `<textarea class="remake-edit__textarea" update:${keyName}></textarea>`;
     }
 
