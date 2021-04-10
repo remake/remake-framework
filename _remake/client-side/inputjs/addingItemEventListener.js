d5199d2f0bba6b908081d7f4f8779c34e58c4894
Aiden Bai <mail.contraband@gmail.com>
2021-04-09 18:06:30 -0700

    Invoke edit popup callback instead of clicking

--- _remake/client-side/inputjs/addingItemEventListener.js
+++ _remake/client-side/inputjs/addingItemEventListener.js
@@ -1,21 +1,21 @@
 import { $ } from '../queryjs';
 import { getAttributeValueAsArray } from '../parse-data-attributes';
 import { ajaxPost } from '../hummingbird/lib/ajax';
-import { findNearest, onAttributeEvent} from '../hummingbird/lib/dom';
+import { findNearest, onAttributeEvent } from '../hummingbird/lib/dom';
 import { isStringANumber } from '../hummingbird/lib/string';
 import { showError } from '../common/show-error';
 import { callSaveFunction } from './onSave';
-import { callOnAddItemCallbacks } from './callbacks';
+import { callOnAddItemCallbacks, openEditCallback } from './callbacks';
 import optionsData from './optionsData';
 const camelCase = require('lodash/camelCase');
 
-function _defaultAddItemCallback ({templateName, listElem, whereToInsert, openEditPrompt}) {
+function _defaultAddItemCallback({ templateName, listElem, whereToInsert, openPopup }) {
   // pass the template name into an endpoint and get the resulting html back
-  ajaxPost("/new", {templateName}, function (ajaxResponse) {
-    let {htmlString, success} = ajaxResponse;
+  ajaxPost('/new', { templateName }, function(ajaxResponse) {
+    let { htmlString, success } = ajaxResponse;
 
     if (!success) {
-      callOnAddItemCallbacks({success: false, templateName, ajaxResponse});
+      callOnAddItemCallbacks({ success: false, templateName, ajaxResponse });
       return;
     }
 
@@ -30,36 +30,62 @@ function _defaultAddItemCallback ({templateName, listElem, whereToInsert, openEd
     // save needs to be called on the list element, not the item, so it doesn't try to save to a non-existent id
     callSaveFunction(listElem);
 
-    let itemElem = whereToInsert === "afterbegin" ? listElem.firstElementChild : listElem.lastElementChild;
+    let itemElem =
+      whereToInsert === 'afterbegin' ? listElem.firstElementChild : listElem.lastElementChild;
 
-    // Click new itemElem so that it opens up the edit prompt
-    if (openEditPrompt) itemElem.click();
-    callOnAddItemCallbacks({success: true, listElem, itemElem, templateName, ajaxResponse});
+    if (openPopup) {
+      const attributeNames = itemElem.getAttributeNames();
+      let editAttribute = null;
+
+      // We need to find the full name of the edit:item attribute so
+      // That we can pass it into the matchingAttribute property
+      // We can't just getAttribute since edit:item's item can vary
+      for (const attributeName of attributeNames) {
+        if (attributeName.startsWith('edit:')) {
+          editAttribute = attributeName;
+          break;
+        }
+      }
+
+      // Brings up the edit popup
+      // We need to pass a matches object because it technically relies on the hummingbird return for the
+      // callback for onAttributeEvent()
+      openEditCallback([
+        {
+          matchingElement: itemElem,
+          matchingAttribute: editAttribute,
+          value: '',
+          eventType: 'click',
+          mathcingPartialAttributeString: 'edit:',
+        },
+      ]);
+    }
+
+    callOnAddItemCallbacks({ success: true, listElem, itemElem, templateName, ajaxResponse });
   });
 }
 
-export default function () {
+export default function() {
   onAttributeEvent({
-    eventTypes: ["click"],
-    partialAttributeStrings: ["new:"],
-    filterOutElemsInsideAncestor: "[disable-events]",
-    callback: ({matchingElement, matchingAttribute}) => {
-      let templateName = camelCase(matchingAttribute.substring("new:".length));
+    eventTypes: ['click'],
+    partialAttributeStrings: ['new:'],
+    filterOutElemsInsideAncestor: '[disable-events]',
+    callback: ({ matchingElement, matchingAttribute }) => {
+      let templateName = camelCase(matchingAttribute.substring('new:'.length));
       // possible values in argArray: top/bottom or some selector
       let argArray = getAttributeValueAsArray(matchingElement, matchingAttribute);
-      let position = argArray.indexOf("top") !== -1 ? "top" : "bottom";
-      let whereToInsert = position === "top" ? "afterbegin" : "beforeend";
-      let selector = argArray.find(arg => arg !== "top" && arg !== "bottom") || "[array]";
+      let position = argArray.indexOf('top') !== -1 ? 'top' : 'bottom';
+      let openPopup = argArray.indexOf('open-popup') !== -1;
+      let whereToInsert = position === 'top' ? 'afterbegin' : 'beforeend';
+      let selector = argArray.find((arg) => arg !== 'top' && arg !== 'bottom') || '[array]';
       // find the nearest element matching the selector (searching through ancestors consecutively)
-      let listElem = findNearest({elem: matchingElement, selector});
-      // Automatically open edit prompt if a single `edit` attr is present
-      let openEditPrompt = matchingElement.hasAttribute('edit');
+      let listElem = findNearest({ elem: matchingElement, selector });
 
       if (!optionsData._defaultAddItemCallback) {
-        _defaultAddItemCallback({templateName, listElem, whereToInsert, openEditPrompt});
+        _defaultAddItemCallback({ templateName, listElem, whereToInsert, openPopup });
       } else {
-        optionsData._defaultAddItemCallback({templateName, listElem, whereToInsert, openEditPrompt});
+        optionsData._defaultAddItemCallback({ templateName, listElem, whereToInsert, openPopup });
       }
-    }
+    },
   });
 }
