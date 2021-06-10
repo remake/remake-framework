bf4256b16089ca103b2caeee70451a161fb07a89
David Miranda <david@storylog.com>
2021-06-09 21:27:33 -0400

    two tests passing! inside jsdom! jsdom is sweet!

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 const test = require("ava");
 import * as Remake from "./_remake/client-side";
+import { JSDOM } from "jsdom";
+const dom = new JSDOM(`<html>
+  <body>
+    <button class="button" aria-expanded="true">Im A Button</button>
+  </body>
+</html>`);
+const body = dom.window.document.body;
 
-test("foo", t => {
-  t.pass();
+test("simple object with key", async t => {
+  body.innerHTML = `<div object key:message="Hello, world!"></div>`;
+  t.deepEqual(Remake.getSaveData(body), { message: "Hello, world!" });
 });
 
-test("bar", async t => {
-  t.deepEqual({ a: 1 }, { a: 1 });
+test("nested object with key", async t => {
+  body.innerHTML = `<div object key="mailbox"><div object key:message="Hello, world!"></div></div>`;
+  t.deepEqual(Remake.getSaveData(body), { mailbox: { message: "Hello, world!" } });
 });
